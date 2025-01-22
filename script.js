f0f8846b6ec67f7cd08098240518371ba625156a
Skyborn <c.skyborn@hotmail.com>
2025-01-22 16:10:55 +0100

    feat!: design header, left sidebar and create algo for displaying books from array
    chose colors and fonts

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
+// Overall
+
 const myLibrary = [];
+const books = document.querySelector(".books");
 
 function Book(title, author, year, pages, read) {
   this.title = title,
@@ -10,20 +13,71 @@ function Book(title, author, year, pages, read) {
 }
 
 function addBookToLibrary(title, author, year, pages, Boolean) {
-  
   createBook = new Book(title, author, year, pages, Boolean);
-  myLibrary.push(createBook);
-  
+  myLibrary.push(createBook); 
 }
 
 function displayBooks() {
-  
-  console.table(myLibrary);
   for (const book of myLibrary) {
-    console.table(book);
+    const divBook = document.createElement("div");
+    divBook.setAttribute("class", "book");
+    books.appendChild(divBook);
+
+    const divTitle = document.createElement("div");
+    divTitle.setAttribute("class", "book-title");
+    divTitle.textContent = book.title;
+    divBook.appendChild(divTitle);
+
+    const divAuthor = document.createElement("div");
+    divAuthor.setAttribute("class", "book-author");
+    divAuthor.textContent = book.author;
+    divBook.appendChild(divAuthor);
+
+    const divYear = document.createElement("div");
+    divYear.setAttribute("class", "book-year");
+    divYear.textContent = book.year;
+    divBook.appendChild(divYear);
+
+    const divPages = document.createElement("div");
+    divPages.setAttribute("class", "book-pages");
+    divPages.textContent = book.pages;
+    divBook.appendChild(divPages);
+
+    const divRead = document.createElement("div");
+    divRead.setAttribute("class", "book-pages");
+    divRead.textContent = book.read;
+    divBook.appendChild(divRead);
+
   }
 }
 
 addBookToLibrary('On the Beach', "Nevil Shute", 1958, 312, 'True');
 addBookToLibrary('The Fellowship of the Ring', "John Ronald Reuel Tolkien", 1954, 423, 'True');
 displayBooks();
+
+// Toggle Book Form 
+
+const addBookButton = document.querySelector('.add-book-button');
+const form = document.querySelector('.form');
+
+
+function toggleForm() {
+  if (form.style.display === 'block') { //if form is seen hide it
+    form.style.display = 'none';
+    addBookButton.style.backgroundColor = 'Indigo';
+    addBookButton.style.color = 'White';
+    addBookButton.style.border = 'None';
+
+  } else {
+    form.style.display = 'block';
+    addBookButton.style.backgroundColor = 'White';
+    addBookButton.style.color = 'Indigo';
+    addBookButton.style.border = '1px solid Indigo';
+  }
+}
+
+addBookButton.addEventListener('click', () => {
+  toggleForm();
+});
+
+// Add Book
